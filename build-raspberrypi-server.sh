a7b1ac64ea6a5d701f121400694f2e69d243a0af
stiles69 <brett.salemink@gmail.com>
2018-06-17 01:44:31 -0500

    update

---
+++ build-raspberrypi-server.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  build-raspberrypi-server.sh
+# 
+#         USAGE:  ./build-raspberrypi-server.sh
+# 
+#   DESCRIPTION:  Builds a raspberrypi server.
+# 
+#       OPTIONS:  None
+#  REQUIREMENTS:  None
+#          BUGS:  None
+#         NOTES:  
+#        AUTHOR:  Brett Salemink (BS), brett.salemink@gmail.com
+#       COMPANY:  Rogue Designs
+#       VERSION:  1.0
+#       CREATED:  12/04/2017 04:02:50 PM CST
+#      REVISION:  
+#===============================================================================
+
+sudo apt-get install devscripts debhelper cmake libldap2-dev libgtkmm-3.0-dev libarchive-dev libcurl4-opens intltool gksu git
+
+mkdir ~/stiles69
+
+cd ~/stiles69
+
+git clone --depth 1 https://github.com/raspberrypi/piserver.git
+
+cd piserver
+debuild -uc -us
+
