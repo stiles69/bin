e9902d650a7d158eb05ac52d22538f934c3bfd13
stiles69 <brett.salemink@gmail.com>
2018-06-21 23:39:32 -0500

    Added transmission-script

---
+++ transmission-extract.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  transmission-extract.sh
+# 
+#         USAGE:  ./transmission-extract.sh 
+# 
+#   DESCRIPTION:  This will extract rar files when done downloading.
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Brett Salemink (), brett.salemink@gmail.com
+#       COMPANY:  Rogue Designs
+#       VERSION:  1.0
+#       CREATED:  06/21/2018 11:38:11 PM CDT
+#      REVISION:  ---
+#===============================================================================
+
+unrar e .
