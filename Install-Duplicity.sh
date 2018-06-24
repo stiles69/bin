b84c870daf9ffd47626f7d06405dee96c5a39800
stiles69 <brett.salemink@gmail.com>
2018-06-24 11:19:27 -0500

    Update

---
+++ Install-Duplicity.sh
@@ -0,0 +1,59 @@
+#!/bin/bash  
+#===============================================================================
+#
+#          FILE: Install-Mega-Cmd.sh
+# 
+#         USAGE: ./Install-Mega-Cmd.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), brett.salemink@gmail.com
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 06/24/2018 10:52
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+# Include Lib
+. ~/lib/sh/funcOS.sh
+
+# Use Function
+#funcOS
+
+RESULT=$(funcOS)
+echo 'The result is '
+echo $RESULT
+
+case $RESULT in
+	
+	"Debian")  
+	echo "Installing For Debian/Based"    
+	sudo apt-get install duplicity
+	;;
+
+	"Raspbian GNU/Linux")  
+	echo "Installing For Debian/Based"    
+	sudo apt-get install duplicity
+	;;
+
+	"Antergos Linux")  
+	echo "Installing For Arch/Based"    
+	yay -S duplicity
+	;;
+	
+	*)
+	echo 'No match can not install'
+	exit
+	;;
+esac
+# Exit
+exit 0 
+
+
+
+
