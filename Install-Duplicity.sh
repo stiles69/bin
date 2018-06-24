e41bfbf9d7751257646e99004e0d87312b6a7ee3
stiles69 <brett.salemink@gmail.com>
2018-06-24 16:24:57 -0500

    Update

--- Install-Duplicity.sh
+++ Install-Duplicity.sh
@@ -19,6 +19,8 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+DetermineOS ()
+{
 # Include Lib
 . ~/lib/sh/funcOS.sh
 
@@ -33,17 +35,20 @@ case $RESULT in
 	
 	"Debian")  
 	echo "Installing For Debian/Based"    
-	sudo apt-get install duplicity
+	sudo apt-get install duplicity keychain
+	Main
 	;;
 
 	"Raspbian GNU/Linux")  
 	echo "Installing For Debian/Based"    
-	sudo apt-get install duplicity
+	sudo apt-get install duplicity keychain
+	Main
 	;;
 
 	"Antergos Linux")  
 	echo "Installing For Arch/Based"    
 	yay -S duplicity
+	Main
 	;;
 	
 	*)
@@ -51,6 +56,34 @@ case $RESULT in
 	exit
 	;;
 esac
+}
+
+Main ()
+{
+
+	# Generate key under SU
+	su
+	~/lib/sh/funcGenerateGPGKey.sh
+	umask 077
+	touch /root/backup.sh /root/duplicity.conf
+	chmod u+x /root/backup.sh
+	ls -la /root/backup.sh /root/.duplicity.conf
+	echo 'Now check the permissions. First one should be:'
+	echo '-rwx------  1 root root 0 2006-01-16 06:47 /root/backup.sh'
+	echo 'The second one:'
+	echo '-rw-------  1 root root 0 2006-01-16 06:47 /root/.duplicity.conf'
+
+	echo 'Now you should have a password protected gpg public/private key pair.'
+	gpg --list-secret-keys
+
+	echo 'You should also have an ssh private/public key pair.'
+	ls -la /root/.ssh
+	
+	echo 'End of Main'
+}
+
+
+
 # Exit
 exit 0 
 
