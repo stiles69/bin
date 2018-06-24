4937eb98b270e8ec3553c8d58037525434456bb5
stiles69 <brett.salemink@gmail.com>
2018-06-24 06:17:46 -0500

    Update

---
+++ Display-OS.sh
@@ -0,0 +1,31 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: Display-OS.sh
+# 
+#         USAGE: ./Display-OS.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), brett.salemink@gmail.com
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 06/24/2018 00:53
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+# Include Lib
+. ~/lib/sh/funcOS.sh
+# Use Function
+#funcOS
+echo 'The Linux Distro is: '
+RESULT=$(funcOS)
+echo $RESULT
+# Exit
+exit 0
+
+
