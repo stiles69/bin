3a38a2004653fca3945f594661a1aba525c892c5
stiles69 <brett.salemink@gmail.com>
2018-06-24 12:57:41 -0500

    Update

---
+++ set-Media.sh
@@ -0,0 +1,53 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: set-Media.sh
+# 
+#         USAGE: ./set-Media.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), brett.salemink@gmail.com
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 06/24/2018 12:56
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
+#===============================================================================
+#   GLOBAL DECLARATIONS
+#===============================================================================
+declare -rx SCRIPT=${0##*/}                     # the name of this script
+declare -rx mkdir='/bin/mkdir'                  # the mkdir(1) command
+
+#===============================================================================
+#   SANITY CHECKS
+#===============================================================================
+if [ -z "$BASH" ] ; then
+	printf "$SCRIPT:$LINENO: run this script with the BASH shell\n" >&2
+	exit 192
+fi
+
+if [ ! -x "$mkdir" ] ; then
+	printf "$SCRIPT:$LINENO: command '$mkdir' not available - aborting\n" >&2
+	exit 192
+fi
+
+#===============================================================================
+#   MAIN SCRIPT
+#===============================================================================
+sudo chown -R brettsalemink:users /mnt/Media
+sudo chmod -R 774 /mnt/Media
+
+echo 'Permissions Changed for /mnt/Media'
+#===============================================================================
+#   STATISTICS / CLEANUP
+#===============================================================================
+exit 0
+
