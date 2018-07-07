a6728678f5142a0f0afd870010cd58defb5b7666
stiles69 <brett.salemink@gmail.com>
2018-07-06 21:11:53 -0500

    Update from Bash

---
+++ Install-catt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: Install-catt.sh
+# 
+#         USAGE: ./Install-catt.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 07/06/2018 21:08
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+. $HOME/lib/sh/funcInstall.sh
+
+Install catt
+
+# == EXIT ==
+exit 0
+
