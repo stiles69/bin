69c80f96b279dc12a36d4f9d3b49eaadb08adc45
stiles69 <brett.salemink@gmail.com>
2018-07-11 21:37:29 -0500

    Update from Bash

--- Walk-Thru-Subdirectories-Example.sh
+++ Walk-Thru-Subdirectories-Example.sh
@@ -20,11 +20,13 @@
 set -o nounset                              # Treat unset variables as an error
 
 shopt -s globstar
-. $HOME/lib/sh/funcClean.sh
+
+. $HOME/lib/sh/funcSanitize.sh
 
 function CleanFilenames ()
 {
-	Clean
+	Sanitize -r "$PWD"
+	wait
 }	# End function
 
 function Walk ()
@@ -43,15 +45,15 @@ do
 		NAME="$(echo "$FILENAME" | cut -d'.' -f1)"
 
 		#echo "FILEXT is "$FILEEXT
-		echo "FILENAME is $FILENAME"
-		echo "NAME =  $NAME"
+		#echo "FILENAME is $FILENAME"
+		#echo "NAME =  $NAME"
 
-		#if [ $FILEEXT = "mp3" ]
-		#then
-		#	NAME="$(echo "$FILENAME" | cut -d'.' -f1)"
-		#	ffmpeg -i "$i" -c:a libfdk_aac -vn "$NAME.m4a"
-		#	rm "$NAME.mp3"
-		#fi
+		if [ $FILEEXT = "mp3" ]
+		then
+			NAME="$(echo "$FILENAME" | cut -d'.' -f1)"
+			ffmpeg -i "$i" -c:a libfdk_aac -vn "./$NAME.m4a"
+			rm "$NAME.mp3"
+		fi
 		echo "================================================="
 	fi
 done
@@ -67,45 +69,3 @@ Main
 
 #== Exit ==
 exit 0
-
-#===============================================================================
-#   MAIN SCRIPT
-#===============================================================================
-mkdir ./Converted
-
-#function DeleteOldFiles ()
-#{
-#
-#	find . -maxdepth 1 -name "*.mp3" -delete
-#}	# end function
-#
-#
-#function CleanUp ()
-#{
-#	DeleteOldFiles
-#	rsync -rvz --progress ./Converted/ ./
-#	wait
-#	sudo rm -r "./Converted"
-#}	# end function
-#
-#function Convert () 
-#{
-#	for i in *.mp3
-#	do 
-#		name=`echo $i | cut -d'.' -f1`
-#		ffmpeg -i "$i" -c:a libfdk_aac -vn "./Converted/$name.m4a"
-#		echo "$name"
-#	done
-#}	# end function
-#
-#function Main ()
-#{
-#	Convert
-#	CleanUp
-#}	# end main
-#
-#Main
-##===============================================================================
-##   STATISTICS / CLEANUP
-##===============================================================================
-#exit 0
