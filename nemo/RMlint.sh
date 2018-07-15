4cce8ac5eee36d983ed9dc10c93095b7b96c21ee
stiles69 <brett.salemink@gmail.com>
2018-07-14 23:02:48 -0500

    manjaro Bash

---
+++ nemo/RMlint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash  
+#===============================================================================
+#
+#          FILE: RMlint.sh
+# 
+#         USAGE: ./RMlint.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 07/14/2018 22:56
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+if [ "$#" -gt 0 ]
+then
+	DIR="$1"
+fi
+
+function Main ()
+{
+	echo "You location is: $NEMO_SCRIPT_CURRENT_URI"
+	rmlint "$DIR"
+}	# end Main
+
+Main
+
+# == EXIT ==
+exit 0
+
