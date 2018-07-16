0159cb9f7965ea86229032f5e156a1bb7e2a312a
stiles69 <brett.salemink@gmail.com>
2018-07-16 01:05:13 -0500

    manjaro Bash

--- Install-Oh-My-Zsh.sh
+++ Install-Oh-My-Zsh.sh
@@ -17,15 +17,69 @@
 #      REVISION:  
 #===============================================================================`
 
-sudo apt-get install zsh
-# Set zsh for user
-sudo chsh -s /bin/zsh brettsalemink
-
-#Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-cp $HOME/bin/.zshrc ~/
-echo "Finished copying .zshrc to home directory. Check ~/.zshrc to ensure PATH is set. Line should read export PATH=$HOME/bin:$PATH and export PATH=$HOME/lib/sh:$PATH"
 
 
+
+. $HOME/lib/sh/funcInstall.sh
+
+
+SOFTWAREINSTALL="zsh"
+SOFTWAREINSTALL2="oh-my-zsh"
+SCRIPTDEFINITION="This will install $SOFTWAREINSTALL and then $SOFTWAREINSTALL2. It will then ask if you want to setup the default rogue settings."
+
+function Proceed ()
+{
+	echo $SCRIPTDEFINITION
+	echo "Do you want to proceed? [Y/n]"
+	read PROCEED
+	case $PROCEED in
+		"Y"|"y")
+		ProceedYes
+		;;
+		"N"|"n")
+		ProceedNo
+		;;
+		*)
+		ProceedYes
+		;;
+	esac
+}	# end function
+
+function ProceedYes ()
+{
+	Install $SOFTWAREINSTALL
+	Install-Oh-My-Zsh
+	FinalZSHSetup
+	echo "Setup Completed!"
+}	# end function
+
+function FinalZSHSetup ()
+{
+	rsync -rvz $HOME/bin/files/home/brettsalemink/ $HOME/
+
+}	# end function
+
+function ProceedNo ()
+{
+	exit 0 # default action is exit for no
+}	# end function
+
+function Install-Oh-My-Zsh ()
+{
+	#Install oh-my-zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	wait
+	# Set zsh for user
+	sudo chsh -s /bin/zsh brettsalemink
+	wait
+}	# end function
+
+function Main ()
+{
+	Proceed
+}	# end Main
+
+Main 
+
+# == Exit ==
 exit 0
