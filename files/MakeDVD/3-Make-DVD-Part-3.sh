e5276ad29815a0785864deab743250326abaeec0
stiles69 <brett.salemink@gmail.com>
2018-07-16 17:35:51 -0500

    pi64 Bash

--- files/MakeDVD/3-Make-DVD-Part-3.sh
+++ files/MakeDVD/3-Make-DVD-Part-3.sh
@@ -38,7 +38,7 @@ function ProceedGenerateISOImage ()
 	read PROCEEDGENERATEISOIMAGE
 	case $PROCEEDGENERATEISOIMAGE in
 		1)
-		GenerateISOImage "$DVDTITLE" "$ISONAME" "OUTPUTDIR"
+		Generate "$DVDTITLE" "$ISONAME"
 		;;
 		2)
 		echo "You will make your own ISO image then. Exiting"
