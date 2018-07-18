c90c46091bd02e7b1cf2b67811f9008ec8ce3388
stiles69 <brett.salemink@gmail.com>
2018-07-18 06:43:25 -0500

    Bash

---
+++ NotifyScript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash  
+#===============================================================================
+#
+#          FILE: NotifyScript.sh
+# 
+#         USAGE: ./NotifyScript.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 07/18/2018 06:39
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+while true
+do    
+	fileName=$(inotifywait -r -e create $HOME/Test | sed -r 's/^.*CREATE(,ISDIR)*\s+(.*)$/\2/g')    
+	echo "New file is $filename"
+done
+
