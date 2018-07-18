1c56c137ae3ba34d963c76175430c71fb2a5ccbf
stiles69 <brett.salemink@gmail.com>
2018-07-18 07:57:09 -0500

    manjaro Bash

--- test/Test-Notify.sh
+++ test/Test-Notify.sh
@@ -1,9 +1,9 @@
-#!/bin/bash  
+#!/bin/bash - 
 #===============================================================================
 #
-#          FILE: NotifyScript.sh
+#          FILE: Test-Notify.sh
 # 
-#         USAGE: ./NotifyScript.sh 
+#         USAGE: ./Test-Notify.sh 
 # 
 #   DESCRIPTION: 
 # 
@@ -13,15 +13,13 @@
 #         NOTES: ---
 #        AUTHOR: Brett Salemink (), admin@roguedesigns.us
 #  ORGANIZATION: Rogue Designs
-#       CREATED: 07/18/2018 06:39
+#       CREATED: 07/18/2018 07:51
 #      REVISION:  ---
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
 
-while true
-do    
-	fileName=$(inotifywait -r -e create $HOME/Test | sed -r 's/^.*CREATE(,ISDIR)*\s+(.*)$/\2/g')    
-	echo "New file"
-done
+cd $HOME/Test
+find . -type f -cmin -5 -exec echo '{}' \;
+
 
