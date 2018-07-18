7e72aecb92751c85e6c3ac12b3eaaea8975d1d52
stiles69 <brett.salemink@gmail.com>
2018-07-18 03:28:15 -0500

    pi64 Bash

---
+++ files/MakeDVD/Run-Complete-DVD.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE: Run-Complete-DVD.sh
+# 
+#         USAGE: ./Run-Complete-DVD.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 07/18/2018 03:15
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+. $HOME/lib/sh/funcGenerateISOImage.sh
+
+VIDEONAME="$1"
+echo "$VIDEONAME"
+
+function ConvertVideo ()
+{
+
+	USAGE="This take one paramter a video file and converts to mpg with ffmpeg it can made into a DVD. 1-Make-DVD-Part-1.sh [FILETOCONVERT]"
+
+	echo "What do you want for the title of the DVD, not the ISO. (No Special Characters or spaces.)?"
+	read DVDTITLE
+	echo "What do want to have for the ISO image. It needs to be all capitals and no special characters?"
+	read ISONAME
+
+	OUTPUTDIR="OUTPUTDIR"
+	if [ ! -d "./OUTPUTDIR" ]
+	then 
+		mkdir ./"$OUTPUTDIR"
+	fi
+
+	if [ "$#" -lt1 ]
+	then
+		echo "$USAGE"
+	else
+		NAME="$(echo "$VIDEONAME" | cut -d'.' -f1)"
+		ffmpeg -i "$VIDEONAME" -target ntsc-dvd -aspect 16:9 "$NAME.mpg"
+	fi
+}	# end ConvertVideo
+
+
+function BuildTSFiles ()
+{
+	OUTPUTDIR="OUTPUTDIR"	
+	mkdir "./$OUTPUTDIR"
+	wait
+
+	for i in *.mpg
+	do
+		NAME="$(echo "$i" | cut -d'.' -f1)"
+		echo "$NAME"
+		dvdauthor -o "$OUTPUTDIR/" -t "$NAME.mpg"
+	done	
+}	# end BuildTSFiles
+
+
+function FinalizeDVD ()
+{
+	dvdauthor -o "OUTPUTDIR" -T
+}	# end Main
+
+function ProceedGenerateISOImage ()
+{
+		Generate "$DVDTITLE" "$ISONAME.iso"
+}
+
+function Generate ()
+{
+	genisoimage -dvd-video -V "$DVDTITLE" -o "$ISONAME" "OUTPUTDIR/"
+}	# end Generate
+
+function Main ()
+{
+	ConvertVideo
+	BuildTSFiles	
+	FinalizeDVD
+	ProceedGenerateISOImage 
+}	# end Main
+
+Main
+#=====EXIT=======
+exit 0
+
