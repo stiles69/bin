dd79eb0728b41ae4e59bf247a800263256e36867
stiles69 <brett.salemink@gmail.com>
2018-07-18 21:11:12 -0500

    manjaro Bash

--- Change-Text-To-PDF.sh
+++ Change-Text-To-PDF.sh
@@ -19,19 +19,22 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+INPUTFILE="$1"
+echo "$INPUTFILE"
+
 function Convert ()
 {
-	INPUTFILE="$1"
 	USAGE="This script takes one parameter, a text file and converts it to a PDF file. Chancge-Text-To-PDF.sh TEXTFILE-TO-CONVERT"
-
-	if [ "$#" -lt 1 ]
+	
+	if [ ! -f "$INPUTFILE" ]
 	then
 		echo "Missing parameter $USAGE"
 		echo "Exiting"
 		exit 1
 	else
 		NAME=`echo $INPUTFILE | cut -d'.' -f1`
-		vim "$INPUTFILE" -c "hardcopy > "NAME".ps | q"; ps2pdf "$NAME".ps
+		vim "$INPUTFILE" -c "hardcopy > "$NAME".ps | q"; ps2pdf "$NAME".ps
+	fi
 }	# end function
 
 function Main ()
