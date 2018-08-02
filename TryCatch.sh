084909f2c7ad1e958a2b0f0a5491191cd0bca347
Brett Salemink <brett.salemink@gmail.com>
2018-08-02 08:45:30 -0500

    manjaro Bash

---
+++ TryCatch.sh
@@ -0,0 +1,50 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: TryCatch.sh
+# 
+#         USAGE: ./TryCatch.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 08/02/2018 08:44
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+function try()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function throw()
+{
+    exit $1
+}
+
+function catch()
+{
+    export ex_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $ex_code
+}
+
+function throwErrors()
+{
+    set -e
+}
+
+function ignoreErrors()
+{
+    set +e
+}
+
+
