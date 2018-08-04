fbb3f196f92e4eb2f6da48947cd896269483b5fd
Brett Salemink <brett.salemink@gmail.com>
2018-08-04 01:22:09 -0500

    Update

--- Zim/Make-Zim-Links.sh
+++ Zim/Make-Zim-Links.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 #===============================================================================
 #
-#          FILE: Find-All-SoftLinks.sh
+#          FILE: Make-Zim-Links.sh
 # 
-#         USAGE: ./Find-All-SoftLinks.sh 
+#         USAGE: ./Make-Zim-Links.sh 
 # 
 #   DESCRIPTION: 
 # 
@@ -13,25 +13,21 @@
 #         NOTES: ---
 #        AUTHOR: Brett Salemink (), admin@roguedesigns.us
 #  ORGANIZATION: Rogue Designs
-#       CREATED: 08/04/2018 01:08
+#       CREATED: 08/04/2018 01:18
 #      REVISION:  ---
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
 
-function Find ()
-{
-	echo "Enter the path to search for symbolic links or . for current dir:"
-	read SEARCHPATH
-	ls -lR "$SEARCHPATH" | grep ^l
-}	# end function
-
 function Main ()
 {
-	Find
+	sudo ln -s $HOME/.local/share/zim/plugins/zimclip
+		
 }	# End Main
 
 Main
 
 #===EXIT===
-exit 0
+exit
+
+
