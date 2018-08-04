d734eda8c8f0b64150d1b82110c3865986d83422
Brett Salemink <brett.salemink@gmail.com>
2018-08-03 22:25:45 -0500

    Manjaro

--- Pull-Bin.sh
+++ Pull-Bin.sh
@@ -22,6 +22,7 @@ set -o nounset                              # Treat unset variables as an error
 SYNCDIR=$HOME/bin
 
 . $HOME/lib/sh/funcDisplayHostname.sh
+. $HOME/lib/sh/funcSetPermissions.sh
 
 HOSTNAME="$(DisplayHostname)"
 
