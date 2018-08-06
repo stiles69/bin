181d137a670636109ba2b176ef7133960e98dcb9
Brett Salemink <brett.salemink@gmail.com>
2018-08-06 18:34:19 -0500

    Update

---
+++ Generate-All-Installed-Packages-Arch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: Generate-All-Installed-Packages-Arch.sh
+# 
+#         USAGE: ./Generate-All-Installed-Packages-Arch.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 08/06/2018 18:30
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+#---------- SOURCED ---------
+
+#----------------------------
+
+#---------- GLOBAL VARIABLES ---------
+LINE=' '
+
+#-------------------------------------
+function Main ()
+{
+	# Native Packages
+	pacman -Qqettn > $HOME/Native-Package-List.txt
+	# Aur Packages
+	pacman -Qqettm > $HOME/Aur-Package-List.txt
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
