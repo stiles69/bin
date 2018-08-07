a909b0bc2333b07ff0cbb488ebbe27500d18166f
Brett Salemink <brett.salemink@gmail.com>
2018-08-07 02:33:40 -0500

    Update

--- allmachines/Pull-Bin-ALL-TEST.sh
+++ allmachines/Pull-Bin-ALL-TEST.sh
@@ -1,5 +1,5 @@
-#!/bin/bash - 
-#===============================================================================
+#!/bin/bash  
+#=========================================================
 #
 #          FILE: Pull-Bin-ALL-TEST.sh
 # 
@@ -15,11 +15,10 @@
 #  ORGANIZATION: Rogue Designs
 #       CREATED: 08/07/2018 02:15
 #      REVISION:  ---
-#===============================================================================
-
+#=========================================================
 set -o nounset                              # Treat unset variables as an error
 #---------- SOURCED ---------
-
+. $HOME/lib/sh/funcRR.sh
 #----------------------------
 
 #---------- GLOBAL VARIABLES ---------
@@ -28,11 +27,20 @@ LINE=' '
 #-------------------------------------
 function Main ()
 {
-
+	rr $HOME/bin/allmachines/Hosts.txt	
 }	# end Main
 
+function rr ()
+{
+	while IFS='' read -r line || [[ -n "$line" ]]; 
+	do    
+		ssh "$line" 'bash -s' < $HOME/bin/update/Update-Manjaro.sh
+	done < "$1"
+}	# end Main
 Main
 
 #===EXIT===
 exit 0
+
+
 
