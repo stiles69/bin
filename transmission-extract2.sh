978a421d163750782c749992eeee054452a13148
stiles69 <brett.salemink@gmail.com>
2018-08-22 00:42:57 -0500

    BASH

--- transmission-extract2.sh
+++ transmission-extract2.sh
@@ -28,10 +28,13 @@ function Main ()
 {
 	find /torrents -name '*.rar' -execdir unrar e -o- {} \; 
 	wait
+	echo "Finished unrar" > ~/Transmission-Extract2.Log
 	find /torrents -name '*.mp4' -name '*.avi' -name '*.mkv' -execdir mv -t /completed {} +;
 	wait
+	echo "Finished Moving to /Completed" >> ~/Transmission-Extract2.Log
 	find /torrents -name '*.mp4' -name '*.avi' -name '*.mkv' -exec mv -t /completed {} +;
 	wait
+	echo "Finished Moving to /Completed" >> ~/Transmission-Extract2.Log
 }	# end Main
 
 Main
