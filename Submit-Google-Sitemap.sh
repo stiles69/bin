03194c76e831b246d8244adcfb23dd950856b2b7
stiles69 <brett.salemink@gmail.com>
2018-09-14 03:26:11 -0500

    manjaro Bash

--- Submit-Google-Sitemap.sh
+++ Submit-Google-Sitemap.sh
@@ -25,16 +25,19 @@ set -o nounset                              # Treat unset variables as an error
   URL = 'https://search.google.com/search-console/sitemaps?resource_id=https%3A%2F%2Fwww.hometown.cab%2F'  
   SITEMAP = 'sitemap.xml'
   JSNAME = 'YPqjbf'
+  INPUTNAME = 'whsOnd.zHQkBf'
 
 #-------------------------------------
 function Main ()
 {
-		# -F for each Field
-        curl -X POST '-F "jsname:$JSNAME" -F "$SITEMAP" "$URL"'
+		# -F for each Field        
+        curl -s -d "jsname=$JSNAME&submit=SUBMIT" <URL> > out.html
 
  #       <input type="text" class="whsOnd zHQkBf" jsname="YPqjbf" autocomplete="off" tabindex="0" aria-label="Enter sitemap URL" autofocus="" autocorrect="off" autocapitalize="off" spellcheck="false" data-initial-value="" badinput="false">
  #       <input type="text" class="whsOnd zHQkBf" jsname="YPqjbf" autocomplete="off" tabindex="0" aria-label="Enter sitemap URL" autofocus="" autocorrect="off" autocapitalize="off" spellcheck="false" data-initial-value="" badinput="false">
  # <div jsname="LwH6nd" class="ndJi5d snByac" aria-hidden="true">Enter sitemap URL</div>
+
+ # https://search.google.com/search-console/sitemaps?resource_id=https%3A%2F%2Fwww.hometown.cab%2F
 }	# end Main
 
 Main
