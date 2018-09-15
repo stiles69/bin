4e343d6a46e96471cc2ce6feb22472f5bbc9a817
stiles69 <brett.salemink@gmail.com>
2018-09-14 23:51:22 -0500

    manjaro Bash

--- Make-DVD/Make-Complete-DVD.sh
+++ Make-DVD/Make-Complete-DVD.sh
@@ -21,96 +21,102 @@
 
 #-------------------------------------
 #---------- GLOBAL VARIABLES ---------
-ASPECTRATIO=" "
-SOURCEVIDEO=" "
-DVDMOVIE=DVDMOVIEDIR
-CURRENTDIR="$PWD"
-DVDISONAME=" "
+
 #-------------------------------------
-function Bootstrap ()
+function Overseer ()
 {
     export VIDEO_FORMAT=NTSC
+    DVDMOVIE=DVDMOVIEDIR
+    CURRENTDIR="$PWD"
+    SOURCEVIDEO="$1"
     
-    CheckDVDMOVIE
-    GetSOURCEVIDEO
-    GetDVDISONAME
-}
-
-function GetDVDISONAME ()
-{
-    echo "What name do you want for the iso, it should be all uppercase and no characters or spaces. ex. MYDVD The extension of iso will be added to your name. [Name of iso: ]"
-    read DVDISONAME
-
-}
-
-function GetASPECTRATIO ()
-{
-    echo "Which aspect ratio do you want to use 16:9 Widescreen or the old 4:3 Normal? [1. 16x9, 2. 4x3]"
-    read ASPECTRATIO
-
-    case "$ASPECTRATIO" in
-	1)
-	ConvertVideo16by9
-	wait
-	;;
-	2)
-	ConvertVideo4by3
-	wait
-	;;
-	*)
-	echo "Unknown response, exiting..."
-	exit 1
-	;;
-    esac
-}
-
-function GetSOURCEVIDEO ()
-{
-    if [ ! -z "$1" ]
-    then
-        echo "No argument supplied for the name of the file you want for the source video. Please enter the name of the video to convert to DVD. The program will assume it is in the same path as you are in now. [Filename:]"
-        read SOURCEVIDEO       
-    else
-        SOURCEVIDEO="$1"        
-    fi
-}
-
-function CheckDVDMOVIE ()
-{
+    # Check if Directory is there
     if [ ! -d "$DVDMOVIE" ]
     then
         mkdir "$DVDMOVIE"
     fi
+  
+    # Check if there is a sourcevideo
+    if [ ! -z "$SOURCEVIDEO" ]
+    then
+        echo "No argument supplied for the name of the file you want for the source video. Please enter the name of the video to convert to DVD. The program will assume it is in the same path as you are in now. [Filename:]"
+        read SOURCEVIDEO                    
+    fi
+   
+    # Get DVDISONAME
+    echo "What name do you want for the iso, it should be all uppercase and no characters or spaces. ex. MYDVD The extension of iso will be added to your name. [Name of iso: ]"
+    read DVDISONAME
+
+    # Get the Aspect Ration
+    echo "Which aspect ratio do you want to use 16:9 Widescreen or the old 4:3 Normal? [1. 16x9, 2. 4x3]"
+    read ASPECTRATIO
+
+    case "$ASPECTRATIO" in
+    1)
+    ConvertVideo16by9 "$SOURCEVIDEO" "$DVDISONAME" "$DVDMOVIE"
+    wait
+    ;;
+    2)
+    ConvertVideo4by3 "$SOURCEVIDEO" "$DVDISONAME" "$DVDMOVIE"
+    wait
+    ;;
+    *)
+    echo "Unknown response, exiting..."
+    exit 1
+    ;;
+    esac
 }
+
 
 function ConvertVideo4by3 ()
 {
+    local SOURCEVIDEO="$1"
+    local DVDISONAME="$2"
+    local DVDMOVIE="$3"
+    echo "Sourcevideo is $SOURCEVIDEO"
     ffmpeg -i "$SOURCEVIDEO" -filter:v "scale='if(gt(a,720/480),720,-1)':'if(gt(a,720/480),-1,480)',pad=w=720:h=480:x=(ow-iw)/2:y=(oh-ih)/2" -target ntsc-dvd "temp.mpg"
     wait
+    AssembleDVDFiles "$SOURCEVIDEO" "$DVDISONAME" "$DVDMOVIE"
 }
 
 function ConvertVideo16by9 ()
 {
+    local SOURCEVIDEO="$1"
+    local DVDISONAME="$2"
+    local DVDMOVIE="$3"
+    echo "The sourcevideo is $SOURCEVIDEO"
     ffmpeg -i "$SOURCEVIDEO" -filter:v "scale='if(gt(a,720/480),720,-1)':'if(gt(a,720/480),-1,480)',pad=w=720:h=480:x=(ow-iw)/2:y=(oh-ih)/2" -aspect 16:9 -target ntsc-dvd "temp.mpg"
     wait
+    AssembleDVDFiles "$SOURCEVIDEO" "$DVDISONAME" "$DVDMOVIE"
 }
 
 function AssembleDVDFiles ()
 {
+    local SOURCEVIDEO="$1"
+    local DVDISONAME="$2"
+    local DVDMOVIE="$3"
     dvdauthor --title -o "$DVDMOVIE" -f "temp.mpg"
     wait
     dvdauthor -T -o "$DVDMOVIE"
     wait
+    GenerateISOFile "$SOURCEVIDEO" "$DVDISONAME" "$DVDMOVIE"
 }
 
 function GenerateISOFile ()
 {
+    local SOURCEVIDEO="$1"
+    local DVDISONAME="$2"
+    local DVDMOVIE="$3"
     genisoimage -dvd-video -o "$DVDISONAME".iso "$DVDMOVIE"
     wait
+    BurnDVD "$SOURCEVIDEO" "$DVDISONAME" "$DVDMOVIE"
 }
 
 function BurnDVD ()
 {
+    local SOURCEVIDEO="$1"
+    local DVDISONAME="$2"
+    local DVDMOVIE="$3"
     echo "$DVDISONAME.iso is ready to be burned, do you want to proceed. [Y/n]"
     read PROCEED
     case "$PROCEED" in
@@ -130,15 +136,7 @@ function BurnDVD ()
 
 function Main ()
 {
-    Bootstrap
-    wait
-    GetASPECTRATIO
-    wait
-    AssembleDVDFiles
-    wait
-    GenerateISOFile
-    wait
-    BurnDVD
+    Overseer    
 }	# end Main
 
 Main
