81fc8bc1ff3411affa938bff61c3a3a0c3f13f12
stiles69 <brett.salemink@gmail.com>
2018-09-16 07:07:49 -0500

    manjaro Bash

--- convert/Convert-Any-Video-To-RaspberryPi-Kodi-mp4.sh
+++ convert/Convert-Any-Video-To-RaspberryPi-Kodi-mp4.sh
@@ -19,15 +19,13 @@
 
 set -o nounset                              # Treat unset variables as an error
 CURRENTDIR="$PWD"
-
+FILENAME="$1"
 function GetFile ()
 {
-	if [ ! -z "$1" ]
+	if [ "$FILENAME" = null ]
 	then
 		echo "Please select the filename. Make sure you spell it exactly and it is in the same path as the $PWD. [ You can also select the filename as a commandline parameter ]"
-		read FILENAME
-	else
-		FILENAME="$1"
+		read FILENAME	
 	fi
 }
 
@@ -42,13 +40,11 @@ function MakeDir ()
 
 function Convert () 
 {
-	
 		name=`echo "$FILENAME" | cut -d'.' -f1`
 		echo $name
 		#ffmpeg -i "$i" -c:a libfdk_aac -vn "./Converted/$name.m4a"
-		ffmpeg -i "$FILENAME" -vcodec libx264 -profile:v high -preset fast -crf 18 -b-pyramid none -acodec ac3 -ab 1536k -scodec copy "HOME/Kodi-Exports/$CURRENTDIR/$name.mp4"
-		wait
-	done
+		ffmpeg -i "$FILENAME" -vcodec h264 -profile:v high -level 4.1 "HOME/Kodi-Exports/$CURRENTDIR/$name.mp4"
+		wait	
 }	# end function
 
 function Main ()
