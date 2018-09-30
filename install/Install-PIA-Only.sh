3687cafca6e00680f86e38aea56153711e14aacb
stiles69 <brett.salemink@gmail.com>
2018-09-29 21:38:46 -0500

    slave3 Bash

--- install/Install-PIA-Only.sh
+++ install/Install-PIA-Only.sh
@@ -27,21 +27,21 @@ set -o nounset                              # Treat unset variables as an error
 function Main ()
 {
 	cd /etc/openvpn
-	sudo su
-	wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
-	unzip openvpn.zip	
-	rm openvpn.zip
-	touch login.conf
-	echo "Please enter your PIA login:"
-	read PIALOGIN
-	echo "Please enter your PIA password:"
-	read PIAPASSWORD
-	echo "$PIALOGIN" >> login.conf
-	echo "$PIAPASSWORD" >> login.conf
-	chmod 400 login.conf
-	cp US\ Chicago.ovpn ./Chicago.conf
-	cat /etc/openvpn/Chicago.conf | sed -e "s/auth-user-pass/auth-user-pass /etc/openvpn/login.conf/" > /etc/openvpn/temp_file
-	mv /etc/openvpn/temp_file /etc/openvpn/Chicago.conf
+	sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
+	sudo unzip openvpn.zip	
+	sudo rm openvpn.zip
+	sudo touch login.conf
+	sudo echo "Please enter your PIA login:"
+	sudo read PIALOGIN
+	sudo echo "Please enter your PIA password:"
+	sudo read PIAPASSWORD
+	sudo echo "$PIALOGIN" >> login.conf
+	sudo echo "$PIAPASSWORD" >> login.conf
+	sudo chmod 400 login.conf
+	sudo touch /etc/openvpn/temp_file
+	sudo cp US\ Chicago.ovpn ./Chicago.conf
+	sudo cat /etc/openvpn/Chicago.conf | sed -e "s/auth-user-pass/auth-user-pass /etc/openvpn/login.conf/" > /etc/openvpn/temp_file
+	sudo mv /etc/openvpn/temp_file /etc/openvpn/Chicago.conf
 	echo "To start openvpn run [sudo openvpn Chicago.conf], or to autostart create a new line under the last AUTOSTART in /etc/default/openvpn with this line [AUTOSTART=Chicago]"
 
 }	# end Main
