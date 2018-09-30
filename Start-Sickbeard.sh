7af9fab170420c50cf5285e8ff71d65827d3bf9f
stiles69 <brett.salemink@gmail.com>
2018-09-29 20:43:35 -0500

    manjaro Bash

--- Start-Sickbeard.sh
+++ Start-Sickbeard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #====================================================
 #
-#          FILE: Install-Sickbeard.sh
+#          FILE: Start-Sickbeard.sh
 # 
-#         USAGE: ./Install-Sickbeard.sh 
+#         USAGE: ./Start-Sickbeard.sh 
 # 
 #   DESCRIPTION: 
 # 
@@ -13,7 +13,7 @@
 #         NOTES: ---
 #        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
 #  ORGANIZATION: Rogue Designs
-#       CREATED: 09/29/2018 20:20
+#       CREATED: 09/29/2018 20:40
 #      REVISION:  ---
 #====================================================
 set -o nounset                              # Treat unset variables as an error
@@ -26,13 +26,7 @@ set -o nounset                              # Treat unset variables as an error
 #-------------------------------------
 function Main ()
 {
-	wget https://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz
-	tar -zxvf Cheetah-2.4.4.tar.gz
-	cd Cheetah-2.4.4
-	sudo python2.7 setup.py install
-	cd $HOME/development/stiles69
-	git clone git://github.com/midgetspy/Sick-Beard.git my-sickbeard-install
-	cd my-sickbeard-install
+	cd $HOME/development/stiles69/my-sickbeard-install
 	python2.7 SickBeard.py
 }	# end Main
 
@@ -40,4 +34,3 @@ Main
 
 #===EXIT===
 exit 0
-
