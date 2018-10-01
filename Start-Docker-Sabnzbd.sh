4a4c6847bed35348587857023b75799eea9ad29c
stiles69 <brett.salemink@gmail.com>
2018-09-30 23:17:08 -0500

    slave1 Bash

--- Start-Docker-Sabnzbd.sh
+++ Start-Docker-Sabnzbd.sh
@@ -27,13 +27,13 @@ set -o nounset                              # Treat unset variables as an error
 function Main ()
 {
 	docker create --name=sabnzbd \
-		-v /home/brettsalemink/development/Docker/lsioarmhf-sabnzbd/config:/config \
+		-v /home/brettsalemink/development/Docker/docker-sabnzbd-armhf/config:/config \
 		-v /torrents:/downloads \
 		-v /torrents/incomplete:/incomplete-downloads \
 		-e PGID=1001 -e PUID=1001 \
 		-e TZ=America/Chicago \
 		-p 8080:8080 -p 9090:9090 \
-		lsioarmhf/sabnzbd
+		stiles/docker-sabnzbd-armhf:latest
 
 
 
