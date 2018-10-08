d2da6d7d40760692fbef888f0fded59782d9094b
stiles69 <brett.salemink@gmail.com>
2018-10-08 02:05:11 -0500

    manjaro Bash

--- Send-Message.sh
+++ Send-Message.sh
@@ -27,7 +27,7 @@ set -o nounset                              # Treat unset variables as an error
 function Main ()
 {
 	port=3333
-	nc -l $port | while read msg; do zenity --info --text "$msg"; done
+	nc master.roguedesigns.us $port
 }	# end Main
 
 Main
