d1b2d2e538c7b8ed88c55055a36957f74277965f
stiles69 <brett.salemink@gmail.com>
2018-10-08 01:04:30 -0500

    manjaro Bash

---
+++ Message-Daemon.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+#====================================================
+#
+#          FILE: Send-Message.sh
+# 
+#         USAGE: ./Send-Message.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 10/08/2018 00:58
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+
+#-------------------------------------
+function Main ()
+{
+	port=3333
+	nc -l $port | while read msg; do zenity --info --text "$msg"; done
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
