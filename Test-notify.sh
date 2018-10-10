362b3c9a53a3d3bf3578d1f7fb2e8845280ebe16
stiles69 <brett.salemink@gmail.com>
2018-10-10 02:08:26 -0500

    manjaro Bash

---
+++ Test-notify.sh
@@ -0,0 +1,36 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Test-notify.sh
+# 
+#         USAGE: ./Test-notify.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 10/10/2018 01:46
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+
+#-------------------------------------
+function Main ()
+{
+	curl https://xdroid.net/api/message?k=u-440890b42fee&t=title&c= content&u=http://roguedesigns.us
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
