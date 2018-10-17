1fa136c6aba3a582a79a7bdc68a77cfc6f067226
stiles69 <brett.salemink@gmail.com>
2018-10-16 21:44:06 -0500

    manjaro Bash

---
+++ convert/Convert-Flac-to-M4A.sh
@@ -0,0 +1,36 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Convert-Flac-to-M4A.sh
+# 
+#         USAGE: ./Convert-Flac-to-M4A.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 10/16/2018 21:41
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+
+#-------------------------------------
+function Main ()
+{
+	find . -name '*.flac' -exec sh -c 'ffmpeg -i "$1" -c:a libfdk_aac -b:a 320k "${1%.flac}.m4a"' _ {} \;
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
