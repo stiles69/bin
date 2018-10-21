d00e3aec88762127c61bd43d57fac68c95591564
stiles69 <brett.salemink@gmail.com>
2018-10-21 03:27:36 -0500

    slave3 Bash

---
+++ Set-RPI-Docker-Permissions.sh
@@ -0,0 +1,43 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Set-RPI-Docker-Permissions.sh
+# 
+#         USAGE: ./Set-RPI-Docker-Permissions.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 10/21/2018 03:20
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+DIR1=$HOME/development/Docker/rpi-docker-transmission-open-vpn
+DIR2=/torrents
+#-------------------------------------
+function Main ()
+{
+	sudo chown -R brettsalemink:docker "$DIR1"
+	sudo chmod -R 774 "$DIR1"
+	sudo chown -R brettsalemink:docker "$DIR2"
+	sudo chmod -R 774 "$DIR2"
+
+	ls -l "$DIR1"
+	ls -l "$DIR2"
+
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
