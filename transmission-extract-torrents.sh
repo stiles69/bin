42402aebff12bc9fd28a2a3015fc4191c783118f
stiles69 <brett.salemink@gmail.com>
2018-10-21 03:38:48 -0500

    master Bash

---
+++ transmission-extract-torrents.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#====================================================
+#
+#          FILE:  transmission-extract.sh
+# 
+#         USAGE:  ./transmission-extract.sh 
+# 
+#   DESCRIPTION:  This will extract rar files when done downloading.
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Brett Salemink (), brett.salemink@gmail.com
+#       COMPANY:  Rogue Designs
+#       VERSION:  1.0
+#       CREATED:  06/21/2018 11:38:11 PM CDT
+#      REVISION:  ---
+#====================================================
+#---------- SOURCED ---------
+
+#----------------------------
+
+#---------- GLOBAL VARIABLES ---------
+DIR="/torrents"
+DIRCOMPLETED="$DIR/completed"
+PARAM1="$1"
+PARAM2="$2"
+#-------------------------------------
+UnrarDir()
+{
+	cd $DIR
+	find . -name '*.rar' -execdir unrar e -o- {} \;
+	wait
+}	# end
+
+MoveDir()
+{
+	find $DIR -name '*.mp4' -exec mv -t "$DIRCOMPLETED" {} +
+	find $DIR -name '*.mkv' -exec mv -t "$DIRCOMPLETED" {} +
+	find $DIR -name '*.avi' -exec mv -t "$DIRCOMPLETED" {} +
+	find $DIR -name '*.mpg' -exec mv -t "$DIRCOMPLETED" {} +
+	find $DIR -name '*.wmv' -exec mv -t "$DIRCOMPLETED" {} +
+	find $DIR -name '*.mpeg' -exec mv -t "$DIRCOMPLETED" {} +
+	find $DIR1 -name '*.flv' -exec mv -t "$DIRCOMPLETED" {} +
+
+}	# end
+
+function SendMessage ()
+{
+	local URGENCY="$1"
+	local ICONPATH="$2"
+	local TITLE="$3"
+	local MSG="$4"
+	
+	ssh brettsalemink@173.29.176.138 -p 58134 "export Display=:0;notify-send '$TITLE' '$MSG' -t 15000 --icon='$ICONPATH'"
+	curl https://xdroid.net/api/message -X POST -d "k=u-440890b42fee" -d "t='$TITLE'" -d "c='$MSG'" -d "u=http://roguedesigns.us"
+}	# end
+
+function Main ()
+{
+	UnrarDir
+	wait
+	MoveDir
+	wait
+
+	#Check $1
+	if [ -z "$PARAM1" ]
+	then
+		TITLE="SLAVE3"
+	else
+		TITLE="$PARAM1"
+	fi
+
+	#Check $2
+	if [ -z "$PARAM2" ]
+	then
+		MSG="Extraction Completed"
+	else
+		MSG="$PARAM2"
+	fi
+
+
+	URGENCY='normal'		# Array OPTIONAL (low normal critical)
+#	EXPIRETIME=6000			# Time in Milliseconds OPTIONAL
+	ICONPATH='dialog-information'	# Path to ICON OPTIONAL or Name of Icon ex. --icon=dialog-information
+#	TITLE				# Title or Summary MANDATORY
+#	MSG				# Actual Message OPTIONAL
+	
+	SendMessage $URGENCY $ICONPATH $TITLE $MSG 
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
