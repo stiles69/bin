0ce744f445ffaaed03aab9450917d708473d39c5
stiles69 <brett.salemink@gmail.com>
2018-10-21 04:35:52 -0500

    master Bash

---
+++ Set-MediaCenter-Permissions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#=======GLOBAL=======
+DIR1="$HOME/development/Docker/docker-mediacenter-armhf"
+DIR2=/torrents
+
+main 
+{
+	sudo chown -R brettsalemink:docker "$DIR1"
+	sudo chmod -R 774 "$DIR1"
+	sudo chown -R brettsalemink:docker "$DIR2"
+	sudo chmod -R 774 "$DIR2"
+
+	ls -l $DIR1
+	ls -l $DIR2
+}
+
+main
+
+exit 0
