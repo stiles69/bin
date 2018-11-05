5ffb389af9949c0881590d12c4ca5767780e3c44
stiles69 <brett.salemink@gmail.com>
2018-11-05 00:59:37 -0600

    manjaro Bash

--- convert/Convert-Any-Video-To-MPG.sh
+++ convert/Convert-Any-Video-To-MPG.sh
@@ -1,5 +1,5 @@
-#!/bin/bash - 
-#===============================================================================
+#!/bin/bash  
+#====================================================
 #
 #          FILE: Convert-Any-Video-To-MPG.sh
 # 
@@ -15,16 +15,11 @@
 #  ORGANIZATION: Rogue Designs
 #       CREATED: 11/05/2018 00:41
 #      REVISION:  ---
-#===============================================================================
+#====================================================
 
 set -o nounset                              # Treat unset variables as an error
 
 VIDEONAME="$1"
-
-function ConvertVideo ()
-{
-	USAGE="This take one paramter a video file and converts to mpg with ffmpeg it can made into a DVD. 1-Make-DVD-Part-1.sh [FILETOCONVERT]"
-	
 
 function Converter ()
 {
@@ -34,11 +29,10 @@ function Converter ()
 
 function Main ()
 {
-	ConvertVideo
+	Converter
 }	# end Main
 
 Main
 
 #== EXIT==
 exit 0
-
