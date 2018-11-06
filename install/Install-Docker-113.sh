32bc53cdad2af2828d965513311b16a04619b27f
stiles69 <brett.salemink@gmail.com>
2018-11-06 06:03:13 -0600

    slave1 Bash

--- install/Install-Docker-113.sh
+++ install/Install-Docker-113.sh
@@ -24,23 +24,54 @@ set -o nounset                              # Treat unset variables as an error
 #---------- GLOBAL VARIABLES ---------
 
 #-------------------------------------
-function Main ()
+function InstallDocker ()
 {
 	curl -sSL https://get.docker.com | sh
 	wait
 	sudo usermod -aG docker brettsalemink
 	wait 
-	sudo apt-get install docker-compose
-	wait
-	echo 'Install done'
+	echo 'Install of Docker completed.'
 	echo 'Possible Kernel Features Required'
-
 	curl -L https://raw.githubusercontent.com/docker/docker/master/contrib/check-config.sh | sh
 	wait
 }	# end Main
 
+function InstallDockerComposeDeb ()
+{
+	sudo apt-get install docker-compose
+	wait
+}
+
+function InstallDockerComposeArch ()
+{
+	sudo pacman -S docker-compose
+	wait
+}
+
+function GetSystem ()
+{
+	echo 'What system are you installing docker-compose too? [1.Debian based, 2.Arch based]'
+	read SYSTEMANSWER
+
+	case $SYSTEMANSWER in
+		1)
+		InstallDockerComposeDeb
+		;;
+		2)
+		InstallDockerComposeArch
+		;;
+	esac
+}
+
+Main ()
+{
+	InstallDocker
+	wait
+	GetSystem
+	wait
+}
+
 Main
 
-#===EXIT===
+#==Exit==
 exit 0
-
