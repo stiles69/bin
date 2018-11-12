60cad779712c0fa25146239bee75397fd7afc993
stiles69 <brett.salemink@gmail.com>
2018-11-12 12:25:25 -0600

    manjaro Bash

--- convert/Any-Video-to-VP9-Webm.sh
+++ convert/Any-Video-to-VP9-Webm.sh
@@ -27,9 +27,9 @@ SOURCE="$1"
 function Main ()
 {
 	
+	mkdir Output
 	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -pass 2 -b:v 1000K -threads 8 -speed 1 \
-  	-tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 \	  -c:a libopus -b:a 64k -f "$SOURCE.webm"
-	  
+  	-tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 \	  -c:a libopus -b:a 64k -f "./Output/$SOURCE.webm"
 } # end Main
 
 Main
