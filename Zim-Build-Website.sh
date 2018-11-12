1f0b59ce83c2645f3eaf6c5f2476bfa8a3f11964
stiles69 <brett.salemink@gmail.com>
2018-11-12 02:19:25 -0600

    manjaro Bash

--- Zim-Build-Website.sh
+++ Zim-Build-Website.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-#=========================================================
+#====================================================
 #
 #          FILE: build.sh
 # 
@@ -11,36 +11,50 @@
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
 #  ORGANIZATION: Rogue Designs
-#       CREATED: 08/03/2018 18:49
+#       CREATED: 11/12/2018 00:08
 #      REVISION:  ---
-#=========================================================
+#====================================================
 set -o nounset                              # Treat unset variables as an error
 
-function SyncCloudToLocal ()
-{
-	rsync -rvz $HOME/brett.salemink@gmail.com/Notes/ $HOME/Notes/
-}	# end function
 
-function BuildWebsite ()
-{	
-	cd $HOME/Notes
-	zim --index
-	zim --export ~/Notes --template=ZeroFiveEight --index-page=index --overwrite --output="$HOME/development/stiles69/zim" 
-}	# end function
+#------------ SOURCED ----------------
 
-function SyncNotesZim ()
-{
-	$HOME/bin/Pull-Zim.sh
-	$HOME/bin/Pull-Notes.sh
-}	# end function
-
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+INPUTDIR=$HOME/Notes
+EXPORTDIR=$HOME/zim-export
+RSYNCDIR=$HOME/development/stiles69/zim
+#-------------------------------------
 function Main ()
 {
-	SyncCloudToLocal
-	BuildWebsite
-	SyncNotesZim
+	sudo rm -r "$EXPORTDIR/build"
+	zim --index "$INPUTDIR"
+	echo "Completed indexing $INPUTDIR."
+	wait
+	cd "$EXPORTDIR"
+	mkdir build 
+	cd build && rm -rf * 
+	cd .. 
+	zim --export --format=html -O --template=Print --output="$EXPORTDIR/build" --index-page=sitemap "$INPUTDIR"
+	wait
+	rsync -rvz "$EXPORTDIR/build/" "$RSYNCDIR"
+	wait
+	cd "$RSYNCDIR"
+	git add .
+	wait
+	git commit -m "Update Zim-Export"
+	wait
+	git push
+	wait
+
+	echo "Completed indexing $INPUTDIR."
+	echo "Completed Website Build from $INPUTDIR to $EXPORTDIR/build"
+	echo "Completed syncing $EXPORTDIR/build to $RSYNCDIR"
+	echo "Completed pushing $RSYNCDIR. Website can be found at https://stiles69.github.io/zim/sitemap.html"
+
+	
 }	# end Main
 
 Main
