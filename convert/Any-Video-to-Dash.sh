4460b9725bfa47db6142cba6b11f92a820341adb
stiles69 <brett.salemink@gmail.com>
2018-11-13 05:44:49 -0600

    manjaro Bash

--- convert/Any-Video-to-Dash.sh
+++ convert/Any-Video-to-Dash.sh
@@ -32,29 +32,30 @@ function Main ()
 	fi
 
 	VP9_DASH_PARAMS="-tile-columns 4 -frame-parallel 1"
-	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -s 160x90 -b:v 250k -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -an -f webm -dash 1 "Output/video_160x90_250k.webm"
+	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -f webm -dash 1 -an -vf scale=160:190 -b:v 250K -dash 1 "Output/video_160x90_250k.webm"
 
-#	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -s 320x180 -b:v 500k -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -an -f webm -dash 1 "Output/video_320x180_500k.webm"
+	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -f webm -dash 1 -an -vf scale=320:180 -b:v 500k -dash 1 "Output/video_320x180_500k.webm"
 
-#	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -s 640x360 -b:v 750k -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -an -f webm -dash 1 "Output/video_640x360_750k.webm"
-
-#	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -s 640x360 -b:v 1000k -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -an -f webm -dash 1 "Output/video_640x360_1000k.webm"
-
-#	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -s 1280x720 -b:v 1500k -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -an -f webm -dash 1 "Output/video_1280x720_500k.webm"
+	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -f webm -dash 1 -an -vf scale=640:360 -b:v 750k -dash 1 "Output/video_640x360_750k.webm"
+	
+	ffmpeg -i "$SOURCE" -c:v libvpx-vp9 -keyint_min 150 -g 150 ${VP9_DASH_PARAMS} -f webm -dash 1 -an -vf scale=1280:720 -b:v 1500k -dash 1 "Output/video_1280x720_500k.webm"
 
 	#Audio
-	ffmpeg -i "$SOURCE" -c:a libvorbis -b:a 192k -vn -f webm -dash 1 "Output/audio_128k.webm"
+	ffmpeg -i "$SOURCE" -vn -acodec libvorbis -ab 128k -dash 1 "Output/audio_128k.webm"
 }	# end Main
 
 function ProduceManifest ()
 {
-	ffmpeg -f webm_dash_manifest \
-		-i "Output/video_160x90_250k.webm" \
+	ffmpeg -f webm_dash_manifest -i "Output/video_160x90_250k.webm" \
+		-f webm_dach_manifest -i "Output/video_320x180_500.webm" \
+		-f webm_dash_manifest -i "Output/video_640x360_750k.webm" \
+		-f webm_dash_manifest -i "Output/video_1280x720_1500k.webm" \
+		-f webm_dash_manifest -i "Output/audio_128k.webm" \
+		-c copy \
+		-map -map 1 -map 2 -map 3 -map 4 \
 		-f webm_dash_manifest \
-		-i "Output/audio_128k.webm" \
-		-c copy -map 0 -map 1 \
-		-f webm_dash_manifest \
-		-adaptation_sets "id=0,streams=0,1 id=1,streams=2" Output/manifest.mpd
+		-adaptation_sets "id=0,streams=0,1,2,3 id=1,streams=4" \
+		"Output/manifest.mpd"
 
 
 }
--- convert/Convert-Flac-to-M4A.sh
+++ convert/Convert-Flac-to-M4A.sh
@@ -28,7 +28,7 @@ function Main ()
 {
 	#find . -name '*.flac' -exec sh -c 'ffmpeg -i "$1" -c:a libfdk_aac -b:a 320k "${1%.flac}.m4a"' _ {} \;
 
-	find . -name '*.flac' -exec sh -c '/usr/bin/ffmpeg-git -i "$1" -c:v copy -c:a libfdk_aac -b:a 384k "${1%.flac}.m4a"' _ {} \;
+	find . -name '*.flac' -exec sh -c '/usr/bin/ffmpeg -i "$1" -c:v copy -c:a libfdk_aac -b:a 384k "${1%.flac}.m4a"' _ {} \;
 }	# end Main
 
 Main
