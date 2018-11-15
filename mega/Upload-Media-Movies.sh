398cb1d4e824d5b69e206f2529f9a0a0e4f13c33
stiles69 <brett.salemink@gmail.com>
2018-11-15 07:33:07 -0600

    Update

---
+++ mega/Upload-Media-Movies.sh
@@ -0,0 +1,44 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Upload-Media-Movies.sh
+# 
+#         USAGE: ./Upload-Media-Movies.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 11/15/2018 07:28
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+DIR1=/mnt/Media/Movies
+DIR2=/media/Movies
+MEGADIR=/Media/Movies
+#-------------------------------------
+function Main ()
+{
+    echo "Beginning upload of $DIR1"
+    mega-put "$DIR1" "$MEGADIR"
+    echo "Completed $DIR1"
+    echo "Beginning upload of $DIR2"
+    mega-put "$DIR2" "$MEGADIR"
+    echo "Completed $DIR2"
+    $HOME/bin/Send-Message.sh 'Manjaro' 'Done Uploading to Mega'
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
