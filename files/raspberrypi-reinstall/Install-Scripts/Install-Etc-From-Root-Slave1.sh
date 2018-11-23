27ed9e8401d73508b62de0b00378713bac3a999c
stiles69 <brett.salemink@gmail.com>
2018-11-22 23:50:10 -0600

    manjaro Bash

---
+++ files/raspberrypi-reinstall/Install-Scripts/Install-Etc-From-Root-Slave1.sh
@@ -0,0 +1,54 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Install-Etc-From-Root-Slave1.sh
+# 
+#         USAGE: ./Install-Etc-From-Root-Slave1.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 11/22/2018 23:46
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+set -e # Needed for sleep
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+
+#-------------------------------------
+function Main ()
+{
+	echo "Installing dhcpcd.conf"
+	sleep 3
+	sudo cp /root/slave1/etc/dhcpcd.conf /etc/
+	wait
+
+	echo "Installing hostname"
+	sleep 3
+	sudo cp /root/slave1/etc/hostname /etc/
+	wait
+
+	echo "Installing hosts"
+	sleep 3
+	sudo cp /root/slave1/etc/hosts /etc/
+	wait
+
+	echo "Installing sshd_config"
+	sleep 3
+	sudo cp /root/slave1/etc/ssh/sshd_config /etc/ssh/
+	wait
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
