c952501ef4ab3f1803b7370099698c0e0d52a4f8
stiles69 <brett.salemink@gmail.com>
2018-11-23 04:51:18 -0600

    manjaro Bash

--- update/Update-All-Vim-In-Files-Install.sh
+++ update/Update-All-Vim-In-Files-Install.sh
@@ -53,6 +53,8 @@ function Main ()
 	echo "Installing Vim Bash Support"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/vim-scripts/bash-support.vim
+	cd "$VIMBUNDLEDIR/bash-support.vim"
+	sudo rm -r .git
 	cp "$BASHSUPPORTTEMPLATES" "$VIMBUNDLEDIR/bash-support.vim/bash-support/templates/"
 	cp "$BASHSUPPORTSH" "$VIMBUNDLEDIR/bash-support.vim/bash-support/rc/"
 	echo "Completed Installing Vim Bash Support"
@@ -61,54 +63,72 @@ function Main ()
 	echo "Installing Vim CSS"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/juleswang/css.vim
+	cd "$VIMBUNDLEDIR/css.vim"
+	sudo rm -r .git
 	echo "Completed Installing Vim CSS"
 	
 	# Vim Dockerfile
 	echo "Installing Vim Dockerfile"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/ekalinin/dockerfile.vim
+	cd "$VIMBUNDLEDIR/dockerfile.vim"
+	sudo rm -r .git
 	echo "Completed Installing Vim Dockerfile"
 	
 	# Vim HTML5
 	echo "Installing Vim HTML5"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/othree/html5.vim
+	cd "$VIMBUNDLEDIR/html5.vim"
+	sudo rm -r .git
 	echo "Completed Installing Vim HTML5"
 
 	# Vim JSLibraries
 	echo "Installing Vim JSLibraries"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/othree/javascript-libraries-syntax.vim
+	cd "$VIMBUNDLEDIR/javascript-libraries-syntax.vim"
+	sudo rm -r .git
 	echo "Completed Installing Vim JSLibraries"
 	
 	# Vim Stylus
 	echo "Installing Vim Stylus"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/wavded/vim-stylus
+	cd "$VIMBUNDLEDIR/vim-stylus"
+	sudo rm -r .git
 	echo "Completed Installing Vim Stylus"
 
 	# Vim Support
 	echo "Installing Vim Support"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/vim-scripts/Vim-Support
+	cd "$VIMBUNDLEDIR/Vim-Support"
+	sudo rm -r .git
 	echo "Completed Installing Vim Support"
 
 	# Vim Vue
 	echo "Installing Vim Vue"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/posva/vim-vue
+	cd "$VIMBUNDLEDIR/vim-vue"
+	sudo rm -r .git
 	echo "Completed Installing Vim Vue"
 
 	# Vim GoLang
 	echo "Installing Vim GoLang"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/fatih/vim-go
+	cd "$VIMBUNDLEDIR/vim-go"
+	sudo rm -r .git
 	echo "Completed Installing Vim GoLang"
 
 	# Vim Python Syntax
 	echo "Installing Vim Python Syntax"
 	cd "$VIMBUNDLEDIR"
 	git clone https://github.com/hdima/python-syntax
+	cd "$VIMBUNDLEDIR/python-syntax"
+	sudo rm -r .git
 	echo "Completed Installing Vim Python Syntax"
 
 	# Clear Old Vim Config
