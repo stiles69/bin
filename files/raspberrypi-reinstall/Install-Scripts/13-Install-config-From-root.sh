0cc9b2d16d8aa41c64052840c921fbba8fd822b9
stiles69 <brett.salemink@gmail.com>
2018-11-22 23:45:22 -0600

    manjaro Bash

---
+++ files/raspberrypi-reinstall/Install-Scripts/13-Install-config-From-root.sh
@@ -0,0 +1,36 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: 13-Install-config-From-root.sh
+# 
+#         USAGE: ./13-Install-config-From-root.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 11/22/2018 23:43
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+
+#-------------------------------------
+function Main ()
+{
+	sudo cp /root/slave1/home/brettsalemink/config /home/brettsalemink/.ssh/
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
