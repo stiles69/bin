4660f339eb9e7f4d6757010847c38dfe34876f8e
stiles69 <brett.salemink@gmail.com>
2018-11-22 23:39:04 -0600

    manjaro Bash

---
+++ files/raspberrypi-reinstall/Install-Scripts/12-Install-Oh-My-Zsh-From-Config.sh
@@ -0,0 +1,56 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: 12-Install-Oh-My-Zsh-From-Config.sh
+# 
+#         USAGE: ./12-Install-Oh-My-Zsh-From-Config.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 11/22/2018 23:31
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+set -e # Need for sleep
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+
+#-------------------------------------
+function Main ()
+{
+	
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	wait
+	sudo cp /root/slave1/home/brettsalemink/.zshrc /home/brettsalemink/
+	wait
+	echo 'Oh My ZSH installed.'
+	sleep 5
+
+	echo "Do you want to check .zshrc? [Y/n]"
+	read ANSWER
+
+	case "$ANSWER" in
+		Y|y)
+		vim /home/brettsalemink/.zshrc
+		;;
+		N|n)
+		exit 0
+		;;
+		*)
+		vim /home/brettsalemink/.zshrc
+		;;
+	esac
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
