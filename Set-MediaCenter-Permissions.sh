14e31524ff9af31fe7504926aa50f91341b30c64
stiles69 <brett.salemink@gmail.com>
2018-11-29 05:22:06 -0600

    master Bash

--- Set-MediaCenter-Permissions.sh
+++ Set-MediaCenter-Permissions.sh
@@ -3,16 +3,21 @@
 #=======GLOBAL=======
 DIR1="$HOME/development/Docker/docker-mediacenter-armhf"
 DIR2=/torrents
-
+DIR3=/mnt/SanDisk
+CHUSER=brettsalemink
+CHGROUP=docker
+CHPERM=774
 function main () 
 {
-	sudo chown -R brettsalemink:docker "$DIR1"
-	sudo chmod -R 774 "$DIR1"
-	sudo chown -R brettsalemink:docker "$DIR2"
-	sudo chmod -R 774 "$DIR2"
+	sudo chown -R $CHUSER:$CHGROUP "$DIR1"
+	sudo chmod -R $CHPERM "$DIR1"
+	sudo chown -R $CHUSER:$CHGROUP "$DIR2"
+	sudo chmod -R $CHPERM "$DIR2"
+	sudo chown -R $CHUSER:$CHGROUP "$DIR3"
 
 	ls -l $DIR1
 	ls -l $DIR2
+	ls -l $DIR3
 }
 
 main
