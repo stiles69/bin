28ce2677865195a58ad8a617b9ec13b8cf12ca93
stiles69 <brett.salemink@gmail.com>
2018-12-15 20:09:22 -0600

    manjaro Bash

---
+++ Download-Bin-Zip-Debian.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+#====================================================
+#
+#          FILE: Download-Bin.sh
+# 
+#         USAGE: ./Download-Bin.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 08/26/2018 18:19
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+
+#---------- GLOBAL VARIABLES ---------
+DOWNLOADDIR=$HOME
+#-------------------------------------
+function Main ()
+{
+	sudo apt-get update -y
+	sudo apt-get install wget -y
+	sudo apt-get install unzip -y
+
+	cd $DOWNLOADDIR
+	wget https://github.com/stiles69/bin/archive/master.zip
+	wait
+	unzip $DOWNLOADDIR/master.zip
+	sudo chmod +x $DOWNLOADDIR/bin-master
+	sudo rm -r $DOWNLOADDIR/master.zip
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
