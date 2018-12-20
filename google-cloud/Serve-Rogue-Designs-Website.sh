417252bba162608933cedcc131713b6b29e966c4
stiles69 <brett.salemink@gmail.com>
2018-12-20 14:04:20 -0600

    manjaro Bash

---
+++ google-cloud/Serve-Rogue-Designs-Website.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#====================================================
+#
+#          FILE: Serve-Hometown-Cab-Project.sh
+# 
+#         USAGE: ./Serve-Hometown-Cab-Project.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 09/14/2018 09:05
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+#------------ SOURCED ----------------
+
+#-------------------------------------
+
+#---------- GLOBAL VARIABLES ---------
+SITEDIR="$1"
+#-------------------------------------
+function Main ()
+{
+	rsync -avz --progress --delete-before "$SITEDIR/" "$HOME/development/GitLab/Rogue-Designs-Google-Cloud-Deployment/Webserver1/files/"
+
+	cd $HOME/development/GitLab/Rogue-Designs-Google-Cloud-Deployment/Webserver1
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
