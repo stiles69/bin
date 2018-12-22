9ae621ce90b5c77d42b5fb54e0e58ce2e3f1bfdf
stiles69 <brett.salemink@gmail.com>
2018-12-22 06:19:06 +0000

    slave2 Bash

---
+++ Set-Emby-Docker-Permissions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#=======GLOBAL=======
+DIR1="$HOME/development/Docker/docker-emby-armhf"
+DIR2=/share1
+DIR3=/mnt/SanDisk
+CHUSER=brettsalemink
+CHGROUP=docker
+CHPERM=774
+function main () 
+{
+	sudo chown -R $CHUSER:$CHGROUP "$DIR1"
+	sudo chmod -R $CHPERM "$DIR1"
+	sudo chown -R $CHUSER:$CHGROUP "$DIR2"
+	sudo chmod -R $CHPERM "$DIR2"
+	sudo chown -R $CHUSER:$CHGROUP "$DIR3"
+
+	ls -l $DIR1
+	ls -l $DIR2
+	ls -l $DIR3
+}
+
+main
+
+exit 0
