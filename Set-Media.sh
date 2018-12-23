ed0232ca6fadb72cb2a23e5fe2020d4fc00e0a1c
stiles69 <brett.salemink@gmail.com>
2018-12-22 20:34:17 -0600

    manjaro Bash

--- Set-Media.sh
+++ Set-Media.sh
@@ -39,60 +39,20 @@ DIR8=/media/Music
 DIRSSL=/media/ssl
 
 USERPLEX=plex
-USEREMBY=emby
-GROUPEMBY=brettsalemink
+USEREMBY=brettsalemink
+GROUPEMBY=emby
 GROUPPLEX=brettsalemink
-
-PERM=770
+GROUPEMEDIA=grpmedia
+PERM=774
 #-------------------------------------
 function Main ()
 {
 	# Emby
-	sudo chown -R $USEREMBY:$GROUPEMBY $DIR0
+	sudo chgrp -R $GROUPMEDIA $DIR0
 	sudo chmod -R $PERM $DIR0
-	sudo chown -R $USEREMBY:$GROUPEMBY $DIR3
-	sudo chmod -R $PERM $DIR3
-	# Plex
-	sudo chown -R $USERPLEX:$GROUPPLEX $DIR1
-	sudo chmod -R $PERM $DIR1
-	sudo chown -R $USERPLEX:$GROUPPLEX $DIR2
-	sudo chmod -R $PERM $DIR2
-	# Emby
-	sudo chown -R $USEREMBY:$GROUPEMBY $DIR4
-	sudo chmod -R $PERM $DIR4
-	sudo chown -R $USEREMBY:$GROUPEMBY $DIR5
-	sudo chmod -R $PERM $DIR5
-	sudo chown -R $USEREMBY:$GROUPEMBY $DIR6
-	sudo chmod -R $PERM $DIR6
-	sudo chown -R $USEREMBY:$GROUPEMBY $DIR7
-	sudo chmod -R $PERM $DIR7
-	sudo chown -R $USEREMBY:$GROUPEMBY $DIR8
-	sudo chmod -R $PERM $DIR8	
-	sudo chown -R $USEREMBY:$GROUPEMBY $DIRSSL
-	sudo chmod -R $PERM $DIRSSL
 	# Emby
 	echo "Permissions Changed for $DIR0"
 	ls -l $DIR0
-	# Plex
-	echo "Permissions Changed for $DIR1"
-	ls -l "$DIR1"
-	echo "Permissions Changed for $DIR2"
-	ls -l "$DIR2"
-	# Emby
-	echo "Permissions Changed for $DIR3"
-	ls -l "$DIR3"
-	echo "Permissions Changed for $DIR4"
-	ls -l "$DIR4"
-	echo "Permissions Changed for $DIR5"
-	ls -l "$DIR5"
-	echo "Permissions Changed for $DIR6"
-	ls -l "$DIR6"
-	echo "Permissions Changed for $DIR7"
-	ls -l $DIR7
-	echo "Permissions Changed for $DIR8"
-	ls -l $DIR8
-	echo "Permissions Changed for $DIRSSL"
-	ls -l $DIRSSL
 
 }	# end Main
 
