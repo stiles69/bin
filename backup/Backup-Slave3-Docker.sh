9f310bb3bad3c9fbd2eebcc3902da5881d5e7dc8
stiles69 <brett.salemink@gmail.com>
2018-12-26 00:59:18 -0600

    slave3 Bash

---
+++ backup/Backup-Slave3-Docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Backup-Docker-Master.sh
+# 
+#         USAGE: ./Backup-Docker-Master.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 12/25/2018 19:30
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+
+#-------------------------------------
+function Main ()
+{
+	duplicity -v 1 --no-encryption /home/brettsalemink/development/Docker/ mega://brett.salemink@gmail.com@mega.co.nz//Data/Backups/Slave3-Docker/
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
