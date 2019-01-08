2468258fb73cccad40e36120383b057b34cbba01
stiles69 <brett.salemink@gmail.com>
2019-01-08 07:44:57 -0600

    Update

--- Send-Message.sh
+++ Send-Message.sh
@@ -21,51 +21,24 @@
 
 #-------------------------------------
 #---------- GLOBAL VARIABLES ---------
-#HOST=localhost
-#PORT=22
-HOST=173.29.176.138
-PORT=60001
-PARAM1="$1"
-PARAM2="$2"
+HOST=10.0.0.11
+USAGE="Usage is funcSendMessage MSG TITLE"
+ICON="/usr/share/icons/roguedesigns/slave-icon-256x256.png"
 #-------------------------------------
 function SendMessage ()
 {
-	local URGENCY="$1"
-	local ICONPATH="$2"
-	local TITLE="$3"
-	local MSG="$4"
-	
-	ssh brettsalemink@$HOST -p $PORT "export Display=:0;notify-send '$TITLE' '$MSG' -t 15000 --icon='$ICONPATH'"
+	local TITLE
+	local MSG
+	sshpass -p "Samsung#2013" ssh brettsalemink@10.0.0.11 'dunstify --icon=/usr/share/icons/roguedesigns/slave-icon-256x256.png "'$TITLE'" "'$MSG'"'
+
 	curl https://xdroid.net/api/message -X POST -d "k=u-440890b42fee" -d "t='$TITLE'" -d "c='$MSG'" -d "u=http://roguedesigns.us"
-}	# end
+   
+
+}	
 
 function Main ()
 {
-	#Check $1
-	if [ -z "$PARAM1" ]
-	then
-		TITLE="$(hostname)"
-	else
-		TITLE="$PARAM1"
-	fi
-
-	#Check $2
-	if [ -z "$PARAM2" ]
-	then
-		echo 'What is the Message you want to send'
-		read MSG
-	else
-		MSG="$PARAM2"
-	fi
-
-
-	URGENCY='normal'		# Array OPTIONAL (low normal critical)
-#	EXPIRETIME=6000			# Time in Milliseconds OPTIONAL
-	ICONPATH='dialog-information'	# Path to ICON OPTIONAL or Name of Icon ex. --icon=dialog-information
-#	TITLE				# Title or Summary MANDATORY
-#	MSG				# Actual Message OPTIONAL
-	
-	SendMessage $URGENCY $ICONPATH $TITLE $MSG 
+	SendMessage "SLAVE2" "Torrent Completed"
 }
 Main
 
