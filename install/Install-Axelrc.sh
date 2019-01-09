ddad4576afb136101947283351a53eb2139616f7
stiles69 <brett.salemink@gmail.com>
2019-01-08 21:26:18 -0600

    manjaro Bash

---
+++ install/Install-Axelrc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: 18-Install-Axel.sh
+# 
+#         USAGE: ./18-Install-Axel.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 12/22/2018 05:36
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+
+#-------------------------------------
+function Main ()
+{
+	sudo cp $HOME/bin/files/etc/axelrc /etc
+	wait
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
