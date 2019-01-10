f099751171964c04cc9c3524e04b1c61106d1ed2
stiles69 <brett.salemink@gmail.com>
2019-01-10 01:39:30 -0600

    manjaro Bash

--- Send-Kodi-Message.sh
+++ Send-Kodi-Message.sh
@@ -31,15 +31,15 @@ USAGE="The correct usage is Send-Kodi-Message.sh MESSAGE"
 #-------------------------------------
 function Main ()
 {
-    echo "Please enter message to send:"
-    read MSG    
-    MESSAGE="${MSG// /\\ }"
-    echo "$MESSAGE"
+#    echo "Please enter message to send:"
+#    read MSG    
+#    MESSAGE="${MSG// /\\ }"
+#    echo "$MESSAGE"
     
-    WGETCOMMAND="http://$KODIHOST:$KODIPORT/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification($MESSAGEALERTTYPE,$MESSAGE,$MESSAGETIME))"
-    wget "$WGETCOMMAND"
+#    WGETCOMMAND="http://$KODIHOST:$KODIPORT/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification($MESSAGEALERTTYPE,$MESSAGE,$MESSAGETIME,$MESSAGEICONPATH))"
+#    wget "$WGETCOMMAND"
 
-curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"This is the title of the message","message":"This is the body of the message"},"id":1}' http://10.0.0.3:8080/jsonrpc {"id":1,"jsonrpc":"2.0","result":"OK"}%}}}
+curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"This is the title of the message","message":"This is the body of the message"},"id":1}' http://10.0.0.3:8080/jsonrpc {"id":1,"jsonrpc":"2.0","result":"OK"}%
 
 
 
