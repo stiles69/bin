2e4c472f8cf82dee42e4c19841e5536a0fec34ba
stiles69 <brett.salemink@gmail.com>
2019-01-21 01:18:52 -0600

    manjaro Bash

---
+++ convert/Convert-Directory-VP9-CQ.sh
@@ -0,0 +1,72 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Convert-Directory-VP9-CQ.sh
+# 
+#         USAGE: ./Convert-Directory-VP9-CQ.sh 
+
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 07/12/2018 06:42
+#      REVISION:  ---
+#====================================================
+
+#== GLOBAL VARS ==
+INPUTDIR="$1"
+OUTPUTDIR="$2"
+
+function CheckVars()
+{
+	if [ ! -d "$INPUTDIR" ]
+	then
+		echo "Correct cli usage is Convert-Directory-Any-Video-To-Kodi-Handbrake.sh InputDirFilePath OutputDirFilePath"
+		echo "Please enter the filepath of the Input Directory?"
+		read INPUTDIR
+	fi
+
+	if [ ! -d "$OUTPUTDIR" ]
+	then
+		
+		echo "Correct cli usage is Convert-Directory-Any-Video-To-Kodi-Handbrake.sh InputDirFilePath OutputDirFilePath"
+		echo "Please enter the filepath of the Output Directory?"
+		read OUTPUTDIR
+	fi	
+}
+
+function MakeDir ()
+{
+	mkdir "$OUTPUTDIR/Converted"
+
+}	# end function
+
+function Convert () 
+{
+	cd "$INPUTDIR"	
+
+	for FILENAME in *
+	do 
+		NAME=`echo "$FILENAME" | cut -d'.' -f1`
+		echo $NAME
+		NEWNAME="$NAME.webm"
+		/usr/bin/ffmpeg -i "$FILENAME" -c:v vp9 -b:v 1000k -minrate 750k -maaxrate 1400k -crf 10 -c:a libvorbis "$OUTPUTDIR/$NEWNAME"		
+		wait
+	done
+}	# end function
+
+function Main ()
+{
+	CheckVars
+	Convert
+}	# end function
+
+Main
+
+# == EXIT ==
+exit 0
