6948c8b18c39a855ee52c0ad3cc0dae04a7384c3
stiles69 <brett.salemink@gmail.com>
2019-02-01 15:51:56 -0600

    slave1 Bash

---
+++ backup/Duplicity-Slave1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Duplicity-Slave1.sh
+# 
+#         USAGE: ./Duplicity-Slave1.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 02/01/2019 15:43
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+BACKUPDIR=/home/brettsalemink
+#-------------------------------------
+function Backup ()
+{
+	duplicity incr --name=slave1 --no-encryption --progress $BACKUPDIR file:///backup 
+}
+
+function MegaSync ()
+{
+
+}
+
+function Main ()
+{
+	Backup
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
