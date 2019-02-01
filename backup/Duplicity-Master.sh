f73702c812a7e6e78c7c11a298ec6e4064a9b54b
stiles69 <brett.salemink@gmail.com>
2019-02-01 16:43:42 -0600

    master Bash

---
+++ backup/Duplicity-Master.sh
@@ -0,0 +1,50 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Duplicity-Slave1.sh
+# 
+#         USAGE: ./Duplicity-Slave1.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 02/01/2019 15:43
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+
+#------------ SOURCED ----------------
+
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+BACKUPDIR=/home/brettsalemink
+#-------------------------------------
+function Backup ()
+{
+	duplicity full -v 1 --no-encryption --include="/" --exclude="**" $BACKUPDIR mega://brett.salemink@gmail.com@mega.co.nz//Root/Backups/Master
+#	duplicity incr --name=slave1 --no-encryption --progress $BACKUPDIR file:///backup 
+}
+
+function MegaSync ()
+{
+	megacopy -l /backup -r /Root/Backups/Slave1 --disable-previews	
+}
+
+function Main ()
+{
+	Backup
+#	wait
+#	MegaSync
+#	wait
+}	# end Main
+
+Main
+
+#===EXIT===
+exit 0
+
