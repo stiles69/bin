aa9c413c1ee3ae4e46d174db79072f22f105b799
stiles69 <brett.salemink@gmail.com>
2019-02-02 10:16:58 -0600

    Update

---
+++ convert/Convert-Directory-VP9-VBR-4-Threads.sh
@@ -0,0 +1,71 @@
+#!/bin/bash  
+#====================================================
+#
+#          FILE: Convert-Directory-Template.sh
+# 
+#         USAGE: ./Convert-Directory-Template.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 07/12/2018 06:42
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+#------------ SOURCED ----------------
+									  
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+INPUTDIR="$1"
+OUTPUTDIR="$2"
+COMMAND1="ffmpeg -i "
+COMMAND2=" -c:v libvpx-vp9 -quality good -cpu-used 0 -crf 5 -qmax 35 -threads 4 -c:a libopus -vbr on -b:a 64k "
+EXT="webm"
+#-------------------------------------
+
+function CheckVars()
+{
+	if [ "$INPUTDIR" == null ]
+	then
+		echo "Correct cli usage is Convert-Directory-Template.sh <InputDirFilePath> <OutputDirFilePath>"
+		echo "Please enter the filepath of the Input Directory?"
+		read INPUTDIR
+	fi
+
+	if [ "$OUTPUTDIR" == null ]
+	then		
+		echo "Correct cli usage is Convert-Directory-Template.sh <InputDirFilePath> <OutputDirFilePath>"
+		echo "Please enter the filepath of the Output Directory?"
+		read OUTPUTDIR
+	fi	
+}
+
+function Convert () 
+{
+	cd "$INPUTDIR"	
+
+	for FILENAME in *
+	do 
+		NAME=`echo "$FILENAME" | cut -d'.' -f1`
+		echo $NAME
+		NEWNAME="$NAME.$EXT"		
+		$(echo "$COMMAND1") "$FILENAME" $(echo "$COMMAND2") "$OUTPUTDIR/$NEWNAME"
+		wait
+	done
+}	# end function
+
+function Main ()
+{
+	CheckVars
+	Convert
+}	# end function
+
+Main
+
+# == EXIT ==
+exit 0
