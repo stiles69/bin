925f66ca76b58dc407ad362ac3ae621e157d3e9e
stiles69 <brett.salemink@gmail.com>
2019-02-02 07:36:36 -0600

    Update

--- convert/Convert-Directory-Chromecast-2-Pass.sh
+++ convert/Convert-Directory-Chromecast-2-Pass.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 #====================================================
 #
-#          FILE: Convert-Directory-x264-aac.sh
+#          FILE: Convert-Directory-Chromecast-2-Pass.sh
 # 
-#         USAGE: ./Convert-Directory-x264-aac.sh 
+#         USAGE: ./Convert-Directory-Chromecast-2-Pass.sh 
 # 
 #   DESCRIPTION: 
 # 
@@ -16,34 +16,31 @@
 #       CREATED: 07/12/2018 06:42
 #      REVISION:  ---
 #====================================================
-
-#== GLOBAL VARS ==
+set -o nounset                              # Treat unset variables as an error
+#------------ SOURCED ----------------
+									  
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
 INPUTDIR="$1"
-OUTPUTDIR="$2"
+OUTPUTDIR="$2"									  
+#-------------------------------------
 
 function CheckVars()
 {
-	if [ ! -d "$INPUTDIR" ]
+	if [ "$INPUTDIR" == null ]
 	then
-		echo "Correct cli usage is Convert-Directory-Any-Video-To-Kodi-Handbrake.sh InputDirFilePath OutputDirFilePath"
+		echo "Correct cli usage is Convert-Directory-Chromecast-2-Pass.sh <InputDirFilePath> <OutputDirFilePath>"
 		echo "Please enter the filepath of the Input Directory?"
 		read INPUTDIR
 	fi
 
-	if [ ! -d "$OUTPUTDIR" ]
-	then
-		
-		echo "Correct cli usage is Convert-Directory-Any-Video-To-Kodi-Handbrake.sh InputDirFilePath OutputDirFilePath"
+	if [ "$OUTPUTDIR" == null ]
+	then		
+		echo "Correct cli usage is Convert-Directory-Chromecast-2-Pass.sh <InputDirFilePath> <OutputDirFilePath>"
 		echo "Please enter the filepath of the Output Directory?"
 		read OUTPUTDIR
 	fi	
 }
-
-function MakeDir ()
-{
-	mkdir "$OUTPUTDIR/Converted"
-
-}	# end function
 
 function Convert () 
 {
@@ -55,7 +52,6 @@ function Convert ()
 		echo $NAME
 		NEWNAME="$NAME.mp4"
 
-#		/usr/bin/ffmpeg -i "$FILENAME" -c:v libx264 -profile:v high -level 5 -crf 18 -maxrate 10M -bufsize 16M -pix_fmt yuv420p -vf "scale-iw*sar:ih, scale='if(gt(iw,ih),min(1280,iw),-1)':'if(gt(iw,ih),-1,min(720,ih))'" -x264opts bframes=3:cabac=1 -movflags faststart -c:a libfdk_aac -b:a 320k -y "$OUTPUTDIR/$NEWNAME"		
 		ffmpeg -i "$FILENAME" -c:v libx264 -profile:v high -level 5 -crf 18 -maxrate 10M -bufsize 16M -pix_fmt yuv420p -vf "scale=iw*sar:ih, scale='if(gt(iw,ih),min(1280,iw),-1)':'if(gt(iw,ih),-1,min(720,ih))'" -x264opts bframes=3:cabac=1 -movflags faststart -c:a libfdk_aac -b:a 320k -y "$OUTPUTDIR/$NEWNAME"
 		wait
 	done
