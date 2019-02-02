f9ff81b9e1ccf9ee64b2c07fd0b560edc0fcc38f
stiles69 <brett.salemink@gmail.com>
2019-02-02 07:23:01 -0600

    Update

---
+++ Test/Test2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+#====================================================
+#
+#          FILE: 
+#
+#         USAGE: ./
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 01/16/2019 02:00
+#      REVISION:  ---
+#====================================================
+set -o nounset                              # Treat unset variables as an error
+#------------ SOURCED ----------------
+                                      
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
+                                      
+#-------------------------------------
+function Main ()
+{
+                                      
+}
+Main
+                                      
+#===EXIT===
+exit 0
+
