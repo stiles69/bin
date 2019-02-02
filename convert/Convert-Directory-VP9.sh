259c4ef9ba3248896ee195d9807d55e02eb7b2f0
stiles69 <brett.salemink@gmail.com>
2019-02-02 08:56:56 -0600

    Update

--- convert/Convert-Directory-VP9.sh
+++ convert/Convert-Directory-VP9.sh
@@ -23,7 +23,7 @@ set -o nounset                              # Treat unset variables as an error
 #---------- GLOBAL VARIABLES ---------
 INPUTDIR="$1"
 OUTPUTDIR="$2"
-COMMAND1="ffmpeg -i $FILENAME"
+COMMAND1="ffmpeg -i "
 COMMAND2=" -c:v libvpx-vp9 -b:v 1000k -minrate 750k -maxrate 1400k -crf 10 -c:a libvorbis "
 COMMAND3=" -vf scale=1280x720 -b:v 1024k -minrate 512k -maxrate 1485k -tile-columns 2 -g 240 -threads 8 \
 		 -quality good -crf 32 -c:v libvpx-vp9 -c:a libopus -pass 1 -speed 4 tos-1280x720-24-30fps.webm && \
@@ -36,14 +36,14 @@ function CheckVars()
 {
 	if [ "$INPUTDIR" == null ]
 	then
-		echo "Correct cli usage is Convert-Directory-Template.sh <InputDirFilePath> <OutputDirFilePath>"
+		echo "Correct cli usage is Convert-Directory-VP9 <InputDirFilePath> <OutputDirFilePath>"
 		echo "Please enter the filepath of the Input Directory?"
 		read INPUTDIR
 	fi
 
 	if [ "$OUTPUTDIR" == null ]
 	then		
-		echo "Correct cli usage is Convert-Directory-Template.sh <InputDirFilePath> <OutputDirFilePath>"
+		echo "Correct cli usage is Convert-Directory-VP9 <InputDirFilePath> <OutputDirFilePath>"
 		echo "Please enter the filepath of the Output Directory?"
 		read OUTPUTDIR
 	fi	
@@ -58,8 +58,8 @@ function Convert ()
 		NAME=`echo "$FILENAME" | cut -d'.' -f1`
 		echo $NAME
 		NEWNAME="$NAME.webm"		
-		$(echo "$COMMAND1") $(echo "$COMMAND2") "$OUTPUTDIR/$NEWNAME"
-		$(echo "$COMMAND1") $(echo "$COMMAND3") "$OUTPUTDIR/$NEWNAME"
+		$(echo "$COMMAND1") "$FILENAME" $(echo "$COMMAND2") "$OUTPUTDIR/$NEWNAME"
+		$(echo "$COMMAND1") "$FILENAME" $(echo "$COMMAND3") "$OUTPUTDIR/$NEWNAME"
 
 		wait
 	done
