37f19c7a910e660557f436fe8ea549efc6754a8b
stiles69 <brett.salemink@gmail.com>
2019-02-02 08:06:03 -0600

    Update

--- convert/Convert-Directory-Chromecast.sh
+++ convert/Convert-Directory-Chromecast.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 #====================================================
 #
-#          FILE: Convert-Directory-x264-aac.sh
+#          FILE: Convert-Directory-Chromecast.sh
 # 
-#         USAGE: ./Convert-Directory-x264-aac.sh 
+#         USAGE: ./Convert-Directory-Chromecast.sh 
 # 
 #   DESCRIPTION: 
 # 
@@ -16,23 +16,28 @@
 #       CREATED: 07/12/2018 06:42
 #      REVISION:  ---
 #====================================================
-
-#== GLOBAL VARS ==
+set -o nounset                              # Treat unset variables as an error
+#------------ SOURCED ----------------
+									  
+#-------------------------------------
+#---------- GLOBAL VARIABLES ---------
 INPUTDIR="$1"
 OUTPUTDIR="$2"
+COMMAND="-c:v libx264 -crf 22 -maxrate 25000 -bufsize 31250 -movflags faststart -profile:v high -level 4.0 -ac 2 -c:a aac"									  
+#-------------------------------------
 
 function CheckVars()
 {
 	if [ "$INPUTDIR" == null ]
 	then
-		echo "Correct cli usage is Convert-Directory-x264-aac.sh <InputDirFilePath> <OutputDirFilePath>"
+		echo "Correct cli usage is Convert-Directory-Chromecast.sh <InputDirFilePath> <OutputDirFilePath>"
 		echo "Please enter the filepath of the Input Directory?"
 		read INPUTDIR
 	fi
 
 	if [ "$OUTPUTDIR" == null ]
 	then		
-		echo "Correct cli usage is Convert-Directory-x264-aac.sh <InputDirFilePath> <OutputDirFilePath>"
+		echo "Correct cli usage is Convert-Directory-Chromecast.sh <InputDirFilePath> <OutputDirFilePath>"
 		echo "Please enter the filepath of the Output Directory?"
 		read OUTPUTDIR
 	fi	
@@ -47,8 +52,7 @@ function Convert ()
 		NAME=`echo "$FILENAME" | cut -d'.' -f1`
 		echo $NAME
 		NEWNAME="$NAME.mp4"
-
-		/usr/bin/ffmpeg -i "$FILENAME" -c:v libx264 -crf 22 -maxrate 25000 -bufsize 31250 -movflags faststart -profile:v high -level 4.0 -ac 2 -c:a aac "$OUTPUTDIR/$NEWNAME"		
+		/usr/bin/ffmpeg -i "$FILENAME" "$COMMAND" "$OUTPUTDIR/$NEWNAME"		
 		wait
 	done
 }	# end function
